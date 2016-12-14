a5990350d0f214b1affaf526aa117fb86ce60705
Alexandradra <alexandra.dubost@gmail.com>
2016-12-14 14:48:53 +0100

    Modal box placed and working yyyyyeeeeeeeaaaaaaaaaaaahhhhhhh

--- scripts/script_home.js
+++ scripts/script_home.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded",
     function() {
-        document.querySelector(".start_btn").addEventListener("click", function() {
+        document.querySelector(".modal-btn").addEventListener("click", function() {
           document.querySelectorAll(".zou")[0].classList.toggle("slider");
           document.querySelectorAll(".zou")[1].classList.toggle("slider");
         });
